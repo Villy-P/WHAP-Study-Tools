8978b725ada9f3f0dd2fae4058d00781b4323286
Villy-P <valeriuspetrini@gmail.com>
2024-02-29 14:07:56 -0600

    Move back button to component

---
+++ src/components/backbtn.svelte
@@ -0,0 +1,4 @@
+<button class="flex justify-center items-center rounded-lg ml-2 mt-2 py-1 px-2 bg-slate-200">
+    <!-- svelte-ignore a11y-invalid-attribute -->
+    <a href="javascript:history.back()">Back</a>
+</button>
--- src/routes/quick-quiz.html/+page.svelte
+++ src/routes/quick-quiz.html/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
     import MenuButton from '../../components/meunubtn.svelte';
+    import BackButton from '../../components/backbtn.svelte';
 
     import '../../app.css'
 </script>
 
-<button class="flex justify-center items-center rounded-lg ml-2 mt-2 py-1 px-2 bg-slate-200">
-    <!-- svelte-ignore a11y-invalid-attribute -->
-    <a href="javascript:history.back()">Back</a>
-</button>
+<BackButton/>
