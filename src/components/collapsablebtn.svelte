672371235304c5127e48278c28dc6533002de623
Villy-P <valeriuspetrini@gmail.com>
2024-02-29 14:40:25 -0600

    Create Collapsable Buttons

---
+++ src/components/collapsablebtn.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let title: string;
+    export let desc: string;
+    export let go: string;
+    export let backgroundImage: string;
+
+    let collapsed: HTMLDivElement;
+
+    function collapse() {
+        if (collapsed.style.maxHeight == "0px")
+            collapsed.style.maxHeight = collapsed.scrollHeight + "px";
+        else
+            collapsed.style.maxHeight = "0px";
+    }
+</script>
+
+<button class="w-full border-t-2 border-b-2 border-black h-fit" style="background-image: {backgroundImage};" on:click={() => collapse()}>
+    <div class="w-full h-full text-center mt-5 mb-5 block">{title}</div>
+</button>
+<div class="w-full overflow-hidden max-h-0 transition-all" bind:this={collapsed}>
+    <p class="px-5 py-4">{desc}</p>
+    <button class="float-right mr-4 bg-sky-500 px-3 py-1 rounded-xl">
+        <a class="w-full h-full" href={go}>Begin Test</a>
+    </button>
+</div>
---
+++ src/routes/by-unit.html/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import CollapsableButton from './../../components/collapsablebtn.svelte';
+    import BackButton from '../../components/backbtn.svelte';
+
+    import '../../app.css'
+</script>
+
+<BackButton/>
+<h1 class="font-bold text-3xl text-center py-6">By Unit</h1>
+<CollapsableButton title="Unit 1 - The Global Tapestry" desc="You'll explore how states formed, expanded, and declined in areas of the world during the period c. 1200–c. 1450 and the related political, social, and cultural developments of that time." go="by-unit.html" backgroundImage=""/>
