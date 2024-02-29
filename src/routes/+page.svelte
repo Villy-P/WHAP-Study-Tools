ea4aa7b0be4c29fdb20a3b9eca24351b1143f498
Villy-P <valeriuspetrini@gmail.com>
2024-02-29 13:43:23 -0600

    Style Header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
     import '../app.css'
 </script>
 
-<h1 class="font-bold text-3xl">AP World History Study Tools</h1>
+<h1 class="font-bold text-3xl text-center py-6">AP World History Study Tools</h1>
 
 <MenuButton title="Quick Quiz" go="quick-quiz.html" backgroundImage=""/>
 <MenuButton title="Flashcards" go="flashcards.html" backgroundImage=""/>
