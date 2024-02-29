878f3d9e22bf4e80d25769356e242c927b1a7219
Villy-P <valeriuspetrini@gmail.com>
2024-02-29 13:39:24 -0600

    Base Menu Button

---
+++ src/components/meunubtn.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let title: string;
+    export let go: string;
+    export let backgroundImage: string;
+</script>
+
+<button class="w-full border-t-2 border-b-2 border-black h-fit" style="background-image: {backgroundImage};">
+    <a href={go} class="w-full h-full text-center mt-5 mb-5 block">{title}</a>
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<h1>AP World History Study Tools</h1>
+<script lang="ts">
+    import MenuButton from '../components/meunubtn.svelte';
+
+    import '../app.css'
+</script>
+
+<h1 class="font-bold text-3xl">AP World History Study Tools</h1>
+
+<MenuButton title="Quick Quiz" go="quick-quiz.html" backgroundImage=""/>
