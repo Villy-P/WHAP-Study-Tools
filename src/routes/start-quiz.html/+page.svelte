d605aea38949a3b07eb10db7aecc004b8a64a94c
Villy-P <valeriuspetrini@gmail.com>
2024-03-02 13:06:23 -0600

    Checkbox items

---
+++ src/routes/start-quiz.html/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import '../../app.css'
+
+    const options = [
+        {
+            id: "mcq",
+            name: "Multiple Choice Questions",
+        }, {
+            id: "order-items",
+            name: "Timeline Ordering"
+        }, {
+            id: "allow-images",
+            name: "Allow Questions with Images"
+        }
+    ]
+</script>
+
+<div class="w-96">
+    <h1 class="text-xl">Options:</h1>
+    {#each options as option}
+        <input type="checkbox" id={option.id}/>
+        <label for={option.id}>{option.name}</label><br>
+    {/each}
+</div>
