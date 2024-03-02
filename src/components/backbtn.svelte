56ee140e7ca3de5687b4d6717c2b8efd471e95da
Villy-P <valeriuspetrini@gmail.com>
2024-03-02 12:36:12 -0600

    Add hover styles to back button

--- src/components/backbtn.svelte
+++ src/components/backbtn.svelte
@@ -1,4 +1,4 @@
-<button class="flex justify-center items-center rounded-lg ml-2 mt-2 py-1 px-2 bg-slate-200">
+<button class="flex justify-center items-center rounded-lg ml-2 mt-2 py-1 px-2 bg-slate-200 hover:bg-slate-300">
     <!-- svelte-ignore a11y-invalid-attribute -->
     <a href="javascript:history.back()">Back</a>
 </button>
