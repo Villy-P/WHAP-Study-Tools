b484fe96cd5fe300a034ae5bcffbb8ee9d7f4715
Villy-P <valeriuspetrini@gmail.com>
2024-03-07 20:45:03 -0600

    Question skeleton

---
+++ src/routes/question.html/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import '../../app.css'
+</script>
+
+<div class="flex flex-col h-screen">
+    <div class="w-full text-center border-black border-b-2 font-bold p-2">Musa I (also known as Mansa Musa) made a notable pilgrimage to Mecca and worked to spread Islam throughout his reign. This is an example of which of the following?</div>
+    <div class="flex-grow overflow-auto flex items-center justify-center">
+        <img class="w-96" alt="data" src="https://wpapp.kaptest.com/study/wp-content/uploads/West_African_Trade_Routes___APWHM.png"/>
+    </div>
+    <div class="w-full flex flex-wrap gap-0 border-t-2 border-black">
+        <div class="bg-red-600    h-40 w-1/2 text-white flex justify-center items-center p-2 cursor-pointer">Forced conversion of conquered populations</div>
+        <div class="bg-blue-600   h-40 w-1/2 text-white flex justify-center items-center p-2 cursor-pointer">The spread of Islam facilitating trade relationships</div>
+        <div class="bg-green-600  h-40 w-1/2 text-white flex justify-center items-center p-2 cursor-pointer">Religious conflict between native African religions</div>
+        <div class="bg-orange-600 h-40 w-1/2 text-white flex justify-center items-center p-2 cursor-pointer">A sectarian split between Sunni and Shi’a in Africa</div>
+    </div>
+</div>
