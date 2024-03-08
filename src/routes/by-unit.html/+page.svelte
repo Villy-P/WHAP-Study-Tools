7525ffd6b7bcae29469350d2b584cee43c1e00d2
Villy-P <valeriuspetrini@gmail.com>
2024-03-07 20:20:11 -0600

    More Options

--- src/routes/by-unit.html/+page.svelte
+++ src/routes/by-unit.html/+page.svelte
@@ -10,5 +10,5 @@
 <BackButton/>
 <h1 class="font-bold text-3xl text-center py-6">By Unit</h1>
 {#each units as unit, i}
-    <CollapsableButton title={`Unit ${i + 1} - ${unit.name}`} desc={unit.desc} go="" backgroundImage=""/>
+    <CollapsableButton title={`Unit ${i + 1} - ${unit.name}`} desc={unit.desc} go="/start-quiz.html" backgroundImage=""/>
 {/each}
--- src/routes/start-quiz.html/+page.svelte
+++ src/routes/start-quiz.html/+page.svelte
@@ -12,13 +12,31 @@
             id: "allow-images",
             name: "Allow Questions with Images"
         }
-    ]
+    ];
+
+    let questionCount: number = 20;
+    let minutes: number = 20;
 </script>
 
-<div class="w-96">
-    <h1 class="text-xl">Options:</h1>
-    {#each options as option}
-        <input type="checkbox" id={option.id}/>
-        <label for={option.id}>{option.name}</label><br>
-    {/each}
+<div class="w-full h-screen flex justify-center items-center flex-col gap-1">
+    <div>
+        <h1 class="text-xl">Options:</h1>
+        {#each options as option}
+            <div>            
+                <input type="checkbox" id={option.id}/>
+                <label for={option.id}>{option.name}</label><br>
+            </div>
+        {/each}
+        <div>
+            <input type="range" min="10" max="100" id="questions" bind:value={questionCount}/>
+            <label for="questions">Question Count ({questionCount})</label>
+        </div>
+        <div>
+            <input type="range" min="10" max="200" id="time" bind:value={minutes}/>
+            <label for="time">{minutes === 200 ? "No Time Limit" : `${minutes} Minutes`}</label>
+        </div>
+        <button class="mr-4 bg-sky-500 px-3 py-1 rounded-xl mb-4">
+            <a class="w-full h-full" href="/">Start</a>
+        </button>
+    </div>
 </div>
