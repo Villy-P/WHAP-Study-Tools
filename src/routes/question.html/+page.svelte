c4af7c295ac46efbc5386d56a06dba4909ca1285
Villy-P <valeriuspetrini@gmail.com>
2024-03-12 20:17:36 -0500

    New Question UI

--- src/routes/question.html/+page.svelte
+++ src/routes/question.html/+page.svelte
@@ -2,15 +2,17 @@
     import '../../app.css'
 </script>
 
-<div class="flex flex-col h-screen">
-    <div class="w-full text-center border-black border-b-2 font-bold p-2">Musa I (also known as Mansa Musa) made a notable pilgrimage to Mecca and worked to spread Islam throughout his reign. This is an example of which of the following?</div>
-    <div class="flex-grow overflow-auto flex items-center justify-center">
-        <img class="w-96" alt="data" src="https://wpapp.kaptest.com/study/wp-content/uploads/West_African_Trade_Routes___APWHM.png"/>
-    </div>
-    <div class="w-full flex flex-wrap gap-0 border-t-2 border-black">
-        <div class="bg-red-600    h-40 w-1/2 text-white flex justify-center items-center p-2 cursor-pointer">Forced conversion of conquered populations</div>
-        <div class="bg-blue-600   h-40 w-1/2 text-white flex justify-center items-center p-2 cursor-pointer">The spread of Islam facilitating trade relationships</div>
-        <div class="bg-green-600  h-40 w-1/2 text-white flex justify-center items-center p-2 cursor-pointer">Religious conflict between native African religions</div>
-        <div class="bg-orange-600 h-40 w-1/2 text-white flex justify-center items-center p-2 cursor-pointer">A sectarian split between Sunni and Shi’a in Africa</div>
+<div class="w-full h-screen flex items-center flex-col gap-1 pt-10">
+    <div class="w-8/12 flex flex-col gap-3">
+        <div class="w-full p-2">Musa I (also known as Mansa Musa) made a notable pilgrimage to Mecca and worked to spread Islam throughout his reign. This is an example of which of the following?</div>
+        <div class="flex-grow overflow-auto flex">
+            <img class="w-96" alt="data" src="https://wpapp.kaptest.com/study/wp-content/uploads/West_African_Trade_Routes___APWHM.png"/>
+        </div>
+        <div class="w-full flex flex-col gap-2">
+            <div class="border-1 border-gray-400 py-2 px-3 w-full rounded-xl flex items-center cursor-pointer">Forced conversion of conquered populations</div>
+            <div class="border-1 border-gray-400 py-2 px-3 w-full rounded-xl flex items-center cursor-pointer">The spread of Islam facilitating trade relationships</div>
+            <div class="border-1 border-gray-400 py-2 px-3 w-full rounded-xl flex items-center cursor-pointer">Religious conflict between native African religions</div>
+            <div class="border-1 border-gray-400 py-2 px-3 w-full rounded-xl flex items-center cursor-pointer">A sectarian split between Sunni and Shi’a in Africa</div>
+        </div>
     </div>
 </div>
