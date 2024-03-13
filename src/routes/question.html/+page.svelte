0a727b0329de5ed72aff162d69b6c768c7d12071
Villy-P <valeriuspetrini@gmail.com>
2024-03-12 20:33:21 -0500

    Add navbar and simplify class names

--- src/routes/question.html/+page.svelte
+++ src/routes/question.html/+page.svelte
@@ -2,6 +2,13 @@
     import '../../app.css'
 </script>
 
+<div class="flex w-full border-b-2 border-gray-600 h-12 items-center">
+    <button class="flex justify-center items-center h-3/4 mx-3 hover:bg-slate-100 p-2 rounded-lg text-sm text-blue-500 font-medium">
+        <a href="/">Exit</a>
+    </button>
+    <p class="text-sm font-semibold">12 / 30</p>
+    <p class="text-sm font-semibold ml-auto mr-3">12:31</p>
+</div>
 <div class="w-full h-screen flex items-center flex-col gap-1 pt-10">
     <div class="w-8/12 flex flex-col gap-3">
         <div class="w-full p-2">Musa I (also known as Mansa Musa) made a notable pilgrimage to Mecca and worked to spread Islam throughout his reign. This is an example of which of the following?</div>
@@ -9,10 +16,10 @@
             <img class="w-96" alt="data" src="https://wpapp.kaptest.com/study/wp-content/uploads/West_African_Trade_Routes___APWHM.png"/>
         </div>
         <div class="w-full flex flex-col gap-2">
-            <div class="border-1 border-gray-400 py-2 px-3 w-full rounded-xl flex items-center cursor-pointer">Forced conversion of conquered populations</div>
-            <div class="border-1 border-gray-400 py-2 px-3 w-full rounded-xl flex items-center cursor-pointer">The spread of Islam facilitating trade relationships</div>
-            <div class="border-1 border-gray-400 py-2 px-3 w-full rounded-xl flex items-center cursor-pointer">Religious conflict between native African religions</div>
-            <div class="border-1 border-gray-400 py-2 px-3 w-full rounded-xl flex items-center cursor-pointer">A sectarian split between Sunni and Shi’a in Africa</div>
+            <div class="question-response">Forced conversion of conquered populations</div>
+            <div class="question-response">The spread of Islam facilitating trade relationships</div>
+            <div class="question-response">Religious conflict between native African religions</div>
+            <div class="question-response">A sectarian split between Sunni and Shi’a in Africa</div>
         </div>
     </div>
 </div>
