f76abc2ba3c8272d50641487acec52048f2e5a0c
Villy-P <valeriuspetrini@gmail.com>
2024-03-27 14:12:22 -0500

    Swap through questions

--- src/routes/question.html/+page.svelte
+++ src/routes/question.html/+page.svelte
@@ -16,22 +16,31 @@
     });
 
     let questionWrapper: HTMLDivElement;
-    let hasSelectedQuestion = false;
+    let selectedAnswer = -1;
 
-    function clickAnswer(answer: EventTarget | null) {
+    function clickAnswer(answer: EventTarget | null, index: number) {
         if (answer == null)
             return;
         for (const question of questionWrapper.children)
             question.classList.remove("bg-slate-200");
         const div = answer as HTMLDivElement;
         div.classList.add("bg-slate-200");
-        hasSelectedQuestion = true;
+        selectedAnswer = index;
     }
 
     let currentQuestion: Question;
+    let correct = 0;
 
     function getNewQuestion() {
         currentQuestion = questions.shift() as Question;
+        correct = 0;
+        selectedAnswer = -1
+        for (const question of questionWrapper.children)
+            question.classList.remove("bg-slate-200");
+    }
+
+    function checkAnswer() {
+        correct = currentQuestion.answer == selectedAnswer ? 1 : 2;
     }
 </script>
 
@@ -54,15 +63,34 @@
                 <img class="w-96" alt="data" src="https://wpapp.kaptest.com/study/wp-content/uploads/West_African_Trade_Routes___APWHM.png"/>
             </div> -->
             <div class="w-full flex flex-col gap-2" bind:this={questionWrapper}>
-                {#each currentQuestion.answers as answer}
-                    <button class="question-response" on:click={(e) => clickAnswer(e.target)}>{answer}</button>
+                {#each currentQuestion.answers as answer, index}
+                    <button class="question-response" on:click={(e) => clickAnswer(e.target, index)}>{answer}</button>
                 {/each}
             </div>
-            {#if hasSelectedQuestion}
-                <button class="flex justify-center items-center h-3/4 ml-auto mx-3 w-fit hover:bg-slate-100 p-2 rounded-lg text-sm text-blue-600 font-medium">
+            {#if selectedAnswer != -1 && correct == 0}
+                <button class="check-ans-btn" on:click={(e) => checkAnswer()}>
                     Check Answer
                 </button>
             {/if}
+            <div class="flex w-full justify-center items-center flex-col">
+                {#if correct == 1}
+                <div class="w-11/12 bg-green-500 rounded-lg text-gray-100">
+                    <p class="p-4 font-semibold">That's the correct answer! Good job</p>
+                </div>
+                <button class="check-ans-btn mr-2 mt-2" on:click={(e) => getNewQuestion()}>
+                    Next Question
+                </button>
+                {/if}
+                {#if correct == 2}
+                    <div class="w-11/12 bg-red-500 rounded-lg text-gray-100">
+                        <p class="p-4 pb-1 font-semibold">That's not the right answer. The correct answer is:</p>
+                        <p class="p-4 pt-0">{currentQuestion.answers[currentQuestion.answer]}</p>
+                    </div>
+                    <button class="check-ans-btn mr-2 mt-2" on:click={(e) => getNewQuestion()}>
+                        Next Question
+                    </button>
+                {/if}
+            </div>
         {/if}
     </div>
 </div>
