dc87cfbd97d0095a711240d0cbba3a0e703fc4c0
Villy-P <valeriuspetrini@gmail.com>
2024-03-27 13:20:09 -0500

    create utilities folder

--- src/routes/question.html/+page.svelte
+++ src/routes/question.html/+page.svelte
@@ -1,19 +1,8 @@
 <script lang="ts">
     import { quickQuiz } from '$lib/stores/QuickQuizStore';
     import { onMount } from 'svelte';
+    import { formatTime } from '../../utilities/time';
     import '../../app.css'
-
-    function formatTime(duration: number) {
-        const hrs = ~~(duration / 3600);
-        const mins = ~~((duration % 3600) / 60);
-        const secs = ~~duration % 60;
-        let ret = "";
-        if (hrs > 0)
-            ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
-        ret += "" + mins + ":" + (secs < 10 ? "0" : "");
-        ret += "" + secs;
-        return ret;
-    }
 
     onMount(() => {
         setInterval(() => {
---
+++ src/utilities/time.ts
@@ -0,0 +1,11 @@
+export function formatTime(duration: number) {
+    const hrs = ~~(duration / 3600);
+    const mins = ~~((duration % 3600) / 60);
+    const secs = ~~duration % 60;
+    let ret = "";
+    if (hrs > 0)
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
