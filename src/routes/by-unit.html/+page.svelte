ba1736c2ffa796bc4aa990989ae2e6544f120f84
Villy-P <valeriuspetrini@gmail.com>
2024-03-30 18:04:34 -0500

    add padding

--- src/routes/by-unit.html/+page.svelte
+++ src/routes/by-unit.html/+page.svelte
@@ -9,7 +9,7 @@
 
 <BackButton/>
 <h1 class="font-bold text-3xl text-center py-6">By Unit</h1>
-<div class="flex gap-3 justify-center flex-wrap">
+<div class="flex gap-3 justify-center flex-wrap pb-5">
     {#each units as unit, i}
         <CollapsableButton title={`Unit ${i + 1} - ${unit.name}`} desc={unit.desc} go="/start-quiz.html" backgroundImage={unit.bgpath}/>
     {/each}
