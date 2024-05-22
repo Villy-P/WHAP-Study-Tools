7efcfbe403bba9731a74f1f7d683c575e3d3e5b4
Villy-P <valeriuspetrini@gmail.com>
2024-05-22 16:57:26 -0500

    Import all units

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,12 +1,141 @@
-import unit11 from './unit1/unit1.1.json';
-import unit12 from './unit1/unit1.2.json';
-import unit48 from './unit4/unit4.8.json';
-import unit50 from './unit5/unit5.0.json';
-import unit71 from './unit7/unit7.1.json';
+import unit11 from './unit1/unit1.1.json'
+import unit12 from './unit1/unit1.2.json'
+import unit13 from './unit1/unit1.3.json'
+import unit14 from './unit1/unit1.4.json'
+import unit15 from './unit1/unit1.5.json'
+import unit16 from './unit1/unit1.6.json'
+import unit21 from './unit2/unit2.1.json'
+import unit22 from './unit2/unit2.2.json'
+import unit23 from './unit2/unit2.3.json'
+import unit24 from './unit2/unit2.4.json'
+import unit25 from './unit2/unit2.5.json'
+import unit26 from './unit2/unit2.6.json'
+import unit27 from './unit2/unit2.7.json'
+import unit31 from './unit3/unit3.1.json'
+import unit32 from './unit3/unit3.2.json'
+import unit33 from './unit3/unit3.3.json'
+import unit34 from './unit3/unit3.4.json'
+import unit41 from './unit4/unit4.1.json'
+import unit42 from './unit4/unit4.2.json'
+import unit43 from './unit4/unit4.3.json'
+import unit44 from './unit4/unit4.4.json'
+import unit45 from './unit4/unit4.5.json'
+import unit46 from './unit4/unit4.6.json'
+import unit47 from './unit4/unit4.7.json'
+import unit48 from './unit4/unit4.8.json'
+import unit51 from './unit5/unit5.1.json'
+import unit52 from './unit5/unit5.2.json'
+import unit53 from './unit5/unit5.3.json'
+import unit54 from './unit5/unit5.4.json'
+import unit55 from './unit5/unit5.5.json'
+import unit56 from './unit5/unit5.6.json'
+import unit57 from './unit5/unit5.7.json'
+import unit58 from './unit5/unit5.8.json'
+import unit59 from './unit5/unit5.9.json'
+import unit510 from './unit5/unit5.10.json'
+import unit61 from './unit6/unit6.1.json'
+import unit62 from './unit6/unit6.2.json'
+import unit63 from './unit6/unit6.3.json'
+import unit64 from './unit6/unit6.4.json'
+import unit65 from './unit6/unit6.5.json'
+import unit66 from './unit6/unit6.6.json'
+import unit67 from './unit6/unit6.7.json'
+import unit68 from './unit6/unit6.8.json'
+import unit71 from './unit7/unit7.1.json'
+import unit72 from './unit7/unit7.2.json'
+import unit73 from './unit7/unit7.3.json'
+import unit74 from './unit7/unit7.4.json'
+import unit75 from './unit7/unit7.5.json'
+import unit76 from './unit7/unit7.6.json'
+import unit77 from './unit7/unit7.7.json'
+import unit78 from './unit7/unit7.8.json'
+import unit79 from './unit7/unit7.9.json'
+import unit81 from './unit8/unit8.1.json'
+import unit82 from './unit8/unit8.2.json'
+import unit83 from './unit8/unit8.3.json'
+import unit84 from './unit8/unit8.4.json'
+import unit85 from './unit8/unit8.5.json'
+import unit86 from './unit8/unit8.6.json'
+import unit87 from './unit8/unit8.7.json'
+import unit88 from './unit8/unit8.8.json'
+import unit89 from './unit8/unit8.9.json'
+import unit91 from './unit9/unit9.1.json'
+import unit92 from './unit9/unit9.2.json'
+import unit93 from './unit9/unit9.3.json'
+import unit94 from './unit9/unit9.4.json'
+import unit95 from './unit9/unit9.5.json'
+import unit96 from './unit9/unit9.6.json'
+import unit97 from './unit9/unit9.7.json'
+import unit98 from './unit9/unit9.8.json'
+import unit99 from './unit9/unit9.9.json'
 
-export const questions = 
-    unit48.questions
-        .concat(unit50.questions)
-        .concat(unit12.questions)
-        .concat(unit71.questions)
-        .concat(unit11.questions);
+export const questions = unit11.questions
+		.concat(unit12.questions)
+		.concat(unit13.questions)
+		.concat(unit14.questions)
+		.concat(unit15.questions)
+		.concat(unit16.questions)
+		.concat(unit21.questions)
+		.concat(unit22.questions)
+		.concat(unit23.questions)
+		.concat(unit24.questions)
+		.concat(unit25.questions)
+		.concat(unit26.questions)
+		.concat(unit27.questions)
+		.concat(unit31.questions)
+		.concat(unit32.questions)
+		.concat(unit33.questions)
+		.concat(unit34.questions)
+		.concat(unit41.questions)
+		.concat(unit42.questions)
+		.concat(unit43.questions)
+		.concat(unit44.questions)
+		.concat(unit45.questions)
+		.concat(unit46.questions)
+		.concat(unit47.questions)
+		.concat(unit48.questions)
+		.concat(unit51.questions)
+		.concat(unit52.questions)
+		.concat(unit53.questions)
+		.concat(unit54.questions)
+		.concat(unit55.questions)
+		.concat(unit56.questions)
+		.concat(unit57.questions)
+		.concat(unit58.questions)
+		.concat(unit59.questions)
+		.concat(unit510.questions)
+		.concat(unit61.questions)
+		.concat(unit62.questions)
+		.concat(unit63.questions)
+		.concat(unit64.questions)
+		.concat(unit65.questions)
+		.concat(unit66.questions)
+		.concat(unit67.questions)
+		.concat(unit68.questions)
+		.concat(unit71.questions)
+		.concat(unit72.questions)
+		.concat(unit73.questions)
+		.concat(unit74.questions)
+		.concat(unit75.questions)
+		.concat(unit76.questions)
+		.concat(unit77.questions)
+		.concat(unit78.questions)
+		.concat(unit79.questions)
+		.concat(unit81.questions)
+		.concat(unit82.questions)
+		.concat(unit83.questions)
+		.concat(unit84.questions)
+		.concat(unit85.questions)
+		.concat(unit86.questions)
+		.concat(unit87.questions)
+		.concat(unit88.questions)
+		.concat(unit89.questions)
+		.concat(unit91.questions)
+		.concat(unit92.questions)
+		.concat(unit93.questions)
+		.concat(unit94.questions)
+		.concat(unit95.questions)
+		.concat(unit96.questions)
+		.concat(unit97.questions)
+		.concat(unit98.questions)
+		.concat(unit99.questions);
