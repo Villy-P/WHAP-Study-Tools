a59d2ec3167238aa033786328cab21de22337f9c
Villy-P <valeriuspetrini@gmail.com>
2024-05-23 20:31:30 -0500

    add topics

--- src/components/meunubtn.svelte
+++ src/components/meunubtn.svelte
@@ -5,5 +5,5 @@
 </script>
 
 <button class="w-40 h-40 border-2 border-black rounded-2xl bg-cover" style="background-image: url({backgroundImage});">
-    <a href={go} class="w-full h-full flex items-center justify-center text-white">{title}</a>
+    <a href={go} class="w-full h-full flex items-center justify-center text-white capitalize">{title}</a>
 </button>
---
+++ src/routes/by-topic.html/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import MenuButton from './../../components/meunubtn.svelte';
+    import BackButton from '../../components/backbtn.svelte';
+
+    import { topics } from '../../data/topics.json';
+
+    import '../../app.css'
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        localStorage.clear();
+    });
+</script>
+
+<BackButton/>
+<h1 class="font-bold text-3xl text-center py-6">By Topic</h1>
+<div class="flex gap-3 justify-center flex-wrap pb-5">
+    {#each topics as topic, i}
+        <MenuButton title={topic.name.replaceAll("-", " ")} go="quick-quiz.html" backgroundImage={topic.image}/>
+    {/each}
+</div>
