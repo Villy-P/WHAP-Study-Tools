d0709e297e95ca4aff7839a279365e6d2e6f2146
Villy-P <valeriuspetrini@gmail.com>
2024-07-08 14:02:28 -0500

    Change Card styling

--- src/components/meunubtn.svelte
+++ src/components/meunubtn.svelte
@@ -8,13 +8,9 @@
     export let backgroundImage: string;
 </script>
 
-<Card img={backgroundImage}>
+<Card img={backgroundImage} href="/" horizontal size="md" class="w-fit">
     <h5 class="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
     <Button on:click={() => {$quickQuiz.currentTopic = title; $quickQuiz.byUnit = false}}>
       <a class="flex" href={go}>Start <ArrowRightOutline class="w-6 h-6 ms-2 text-white" /></a>
     </Button>
 </Card>
-<!-- 
-<button class="w-40 h-40 border-2 border-black rounded-2xl bg-cover" style="background-image: url({backgroundImage});" on:click={() => {$quickQuiz.currentTopic = title; $quickQuiz.byUnit = false}}>
-    <a href={go} class="w-full h-full flex items-center justify-center text-white capitalize">{title}</a>
-</button> -->
