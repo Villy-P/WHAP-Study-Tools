4d9d8c77bc809363db9b73bb7f35dd3b2054fcc0
Villy-P <valeriuspetrini@gmail.com>
2024-07-08 14:03:55 -0500

    Update Back Button

--- src/components/backbtn.svelte
+++ src/components/backbtn.svelte
@@ -1,4 +1,5 @@
-<button class="flex justify-center items-center rounded-lg ml-2 mt-2 py-1 px-2 bg-slate-200 hover:bg-slate-300">
-    <!-- svelte-ignore a11y-invalid-attribute -->
-    <a href="javascript:history.back()">Back</a>
-</button>
+<script>
+    import { Button } from "flowbite-svelte";
+</script>
+
+<Button class="ml-2 mt-2" color="light" href="javascript:history.back()">Back</Button>
