31442668ea842828d73cc46f16d8d417702d9c89
Villy-P <valeriuspetrini@gmail.com>
2024-09-11 18:57:01 -0500

    docs: `shuffleArray`

--- src/utilities/random.ts
+++ src/utilities/random.ts
@@ -1,3 +1,8 @@
+/**
+ * Shuffles any type of array (in place)
+ * 
+ * @param array The array to shuffle
+ */
 export function shuffleArray<T>(array: T[]) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
