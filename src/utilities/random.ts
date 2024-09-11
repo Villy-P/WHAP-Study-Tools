2ca909297ecff96bf4b621b4bf43b3c3c5ece84b
Villy-P <valeriuspetrini@gmail.com>
2024-09-11 18:52:46 -0500

    fix: convert `shuffleArray` to generic

--- src/utilities/random.ts
+++ src/utilities/random.ts
@@ -1,4 +1,4 @@
-export function shuffleArray(array: any[]) {
+export function shuffleArray<T>(array: T[]) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         const temp = array[i];
