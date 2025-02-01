b2c4e89952c432fbed861fe138de96b2fd4bc439
Villy-P <valeriuspetrini@gmail.com>
2025-01-31 21:40:07 -0600

    feat: add SEO component with dynamic metadata and structured data support

---
+++ src/components/SEO.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+
+    const websiteName = "https://www.valerius-petrini.vercel.app";
+  
+    interface Props {
+        title?: string;
+        description?: string;
+        image?: any;
+    }
+
+    let { title = "Home | Example.com", description = "“Description of your website.”", image = `${websiteName}/favicon.png` }: Props = $props();
+</script>
+  
+<svelte:head>
+    <title>{title}</title>
+    <meta name="description" content={description}>
+    <meta property="og_site_name" content={websiteName}>
+    <meta property="og:url" content="{websiteName}{$page.url.pathname.toString()}">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="{title}">
+    <meta property="og:description" content={description}>
+    <meta property="og:image" content={image}>
+  
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content={websiteName}>
+    <meta property="twitter:url" content="{websiteName}{$page.url.pathname.toString()}">
+    <meta name="twitter:title" content="{title}">
+    <meta name="twitter:description" content={description}>
+    <meta name="twitter:image" content={image}>
+    {@html `  <script type="application/ld+json">{
+     "@context": "https://schema.org",
+     "@type": "Website",
+     "name": "${title} | example.com,
+     "url": "https//www.example.com${$page.url.pathname}",
+     "logo": ${image}  }</script>`}
+</svelte:head>
