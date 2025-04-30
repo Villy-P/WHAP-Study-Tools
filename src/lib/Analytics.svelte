c7656930d117701bbe277b23b2981cf3f12cdd79
Villy-P <valeriuspetrini@gmail.com>
2025-04-29 21:51:08 -0500

    feat: add Google Analytics integration for page tracking

---
+++ src/lib/Analytics.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+  
+    $: {
+        if (typeof gtag !== 'undefined') {
+            gtag('config', 'MEASUREMENT_ID', {
+                page_title: document.title,
+                page_path: $page.url.pathname,
+            })
+        }
+    }
+
+    const MEASUREMENT_ID = "G-M5CYWKTJF7";
+    const ANALYTICS_URL = "https://www.googletagmanager.com/gtag/js?id=" + MEASUREMENT_ID;
+</script>
+  
+<svelte:head>
+    <script async src={ANALYTICS_URL}></script>
+    <script>
+        window.dataLayer = window.dataLayer || []
+        const MEASUREMENT_ID = "G-M5CYWKTJF7"
+    
+        function gtag() {
+            dataLayer.push(arguments)
+        }
+    
+        gtag('js', new Date())
+        gtag('config', MEASUREMENT_ID)
+    </script>
+</svelte:head>
